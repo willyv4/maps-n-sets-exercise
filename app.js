db2ba41deba1400be4b85c351d005937f54101b8
William Valadez <billy.valadez@icloud.com>
2023-01-23 18:20:42 -0500

    solved

---
+++ app.js
@@ -0,0 +1,27 @@
+new Set([1, 1, 2, 2, 3, 4]); // [1,2,3,4]
+[...new Set("referee")].join(""); // "ref"
+
+let m = new Map();
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false); // [[[1,2,3], true]],[[1,2,3], false]]]
+
+const hasDuplicate = (arr) => {
+  let arrCopy = [...new Set(arr)];
+  return arr.length !== arrCopy.length ? true : false;
+};
+
+function vowelCount(str) {
+  let vowels = "aeiou";
+  const myMap = new Map();
+  for (let char of str) {
+    let letters = char.toLowerCase();
+    if (vowels.includes(letters)) {
+      if (myMap.has(letters)) {
+        myMap.set(letters, myMap.get(letters) + 1);
+      } else {
+        myMap.set(letters, 1);
+      }
+    }
+  }
+  return myMap;
+}
